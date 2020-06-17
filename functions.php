42a9df0fe5138baca3583bd8b80a15dec8ad1fec
graveyard-monk <66170993+graveyard-monk@users.noreply.github.com>
2020-06-18 01:27:11 +0200

    creating files and some updates

--- functions.php
+++ functions.php
@@ -120,38 +120,33 @@ function petrock_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'petrock_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'petrock_content_width', 1140 );
 }
 add_action( 'after_setup_theme', 'petrock_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function petrock_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'petrock' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'petrock' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'petrock_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
 function petrock_scripts() {
+	wp_enqueue_style('petrock-bss-css' , get_template_directory_uri() . '/dist/css/bootstrap.min.css');
+
+	wp_enqueue_style('petrock-fontawesome' , get_template_directory_uri() . '/fonts/font-awesome/css/fontawesome.min.css');
+	wp_enqueue_script('popper');
+
 	wp_enqueue_style( 'petrock-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'petrock-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'petrock-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_register_script('popper', 'https://unpkg.com/@popperjs/core@2.4.2/dist/umd/popper.min.js', false, '', true);
+
+	wp_enqueue_script( 'petrock-tether', get_template_directory_uri() .
+	'/src/js/tether.js', array(),  _S_VERSION, true );
+
+	wp_enqueue_script( 'petrock-bootstrap', get_template_directory_uri() .
+	'/src/js/bootstrap.min.js', array('jquery'), _S_VERSION, true );
+
+	wp_enqueue_script( 'petrock-bootstrap-hover', get_template_directory_uri() .
+	'/src/js/bootstrap-hover.js', array('jquery'),  _S_VERSION, true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
